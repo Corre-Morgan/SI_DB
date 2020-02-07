0bf44721c0a638f955436152b520792cc7a31b98
vincent-alamelle <vincent.alamelle@etu.univ-amu.fr>
2020-02-07 16:36:02 +0100

    Wid

---
+++ src/php/accueil.php
@@ -0,0 +1,23 @@
+<?php
+echo '<div class="menu">
+          <a href="">Article</a>
+          <a href="">Contest</a>
+          <a href="">Etablishment</a>
+          <a href="">Facility</a>
+          <a href="">Horse</a>
+          <a href="">Horse_issue</a>
+          <a href="">Issue</a>
+          <a href="">Item</a>
+          <a href="">Log</a>
+          <a href="">Newspaper</a>
+          <a href="">Newspaper_article</a>
+          <a href="">Prize</a>
+          <a href="">Riding_center</a>
+          <a href="">Riding_club</a>
+          <a href="">Riding_club_subscriber</a>
+          <a href="">Task</a>
+          <a href="">Transaction</a>
+          <a href="">User</a>
+      </div>';
+
+
